6bbba00b17b31b5e792ab8162806ef2e2948a1e2
mdcrowson <marissa.crowson23@gmail.com>
2025-02-18 11:16:51 -0600

    add modal functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 // Global Variables //
 const randomUserUrl = 'https://randomuser.me/api/?results=12&inc=name,email,location,phone,dob,picture';
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+const overlay = document.querySelector('.overlay');
+const modalContent = document.querySelector('.modal-content');
+const modalClose = document.querySelector('.modal-close');
+
 let employees = [];
 
 // Fetch data
@@ -14,7 +18,7 @@ async function getUsers(url) {
 
         const data = await response.json();
         console.log(data);
-        employees = data;
+        employees = data.results;
         displayEmployees(data);
         return data;
     } catch (error) {
@@ -24,7 +28,7 @@ async function getUsers(url) {
 
 // Display data
 function displayEmployees(data){
-    const employeesHTML = employees.results
+    const employeesHTML = employees
         .map( 
             (employee, index) => `
                 <div class="card" data-index="${index}">
@@ -46,6 +50,46 @@ function displayEmployees(data){
 
 // Call function
 getUsers(randomUserUrl);
+
+
+// Event Listeners
+
+    // Display modal with clicked employee's info
+container.addEventListener('click', (event) => {
+    const employeeCard = event.target.closest('.card');
+    if(!employeeCard) return
+    
+    const employeeIndex = employeeCard.dataset.index;
+    const employee = employees[employeeIndex];
+
+    console.log(employee);
+    const birthday = new Date(employee.dob.date);
+    const birthdayFormatted = birthday.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      });
+
+    modalContent.innerHTML = `
+                <img class="modal-img" src="${employee.picture.large}" alt="">
+                <h2>${employee.name.first} ${employee.name.last}</h2>
+                <p>${employee.email}</p>
+                <p>${employee.location.city}</p>
+                <div><hr></div>
+                <p>${employee.phone}</p>
+                <p>${employee.location.street.number} ${employee.location.street.name} ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                <p>Birthday: ${birthdayFormatted}</p>
+            `;
+    overlay.classList.remove('hidden');
+});
+
+    // Close button on modal
+modalClose.addEventListener('click', (event) => {
+    overlay.classList.add('hidden');
+});
+
+
+
 
 
 
